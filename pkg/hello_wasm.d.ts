0587390604699bfaffa608483ad7c5416eee98bb
hyuckkim <monkeyhyuck@naver.com>
2022-02-20 16:45:09 +0900

    pkg files

---
+++ pkg/hello_wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function greet(name: string): void;
+/**
+* @returns {number}
+*/
+export function ten(): number;
