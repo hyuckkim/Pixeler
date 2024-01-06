2765cb54b40865c416098b5eeb2e6768896f4e9a
hyuckkim <monkeyhyuck@gmail.com>
2024-01-06 13:35:42 +0900

    feat: add gamma setting

--- wasmworker.ts
+++ wasmworker.ts
@@ -5,7 +5,8 @@ onmessage = function(e) {
     const img : ImageData = e.data[0];
     const pixelno : number = e.data[1];
     const dithering : number = e.data[2];
+    const gamma : number = e.data[3];
     
-    var quantized = rust.quantize(img.data, img.width, img.height, pixelno, dithering);
+    var quantized = rust.quantize(img.data, img.width, img.height, pixelno, dithering, gamma);
     this.postMessage(quantized);
 }
