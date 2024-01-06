571113b5efe5969c4d95ee26235c91078a7053f7
hyuckkim <monkeyhyuck@gmail.com>
2024-01-06 10:40:55 +0900

    refactor: .gitignore 추가 및 생성되는 파일 삭제

--- wasmworker.js
+++
@@ -1,9 +0,0 @@
-import * as rust from "./pkg/palette_png.js";
-rust.default();
-onmessage = function (e) {
-    const img = e.data[0];
-    const pixelno = e.data[1];
-    const dithering = e.data[2];
-    var quantized = rust.quantize(img.data, img.width, img.height, pixelno, dithering);
-    this.postMessage(quantized);
-};
